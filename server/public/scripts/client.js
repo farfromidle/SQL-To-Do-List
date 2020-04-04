75230dd0414a7d3a824d407df94c079e6d336b66
Derrick Idleburg Jr <derrick@primeacademy.io>
2020-04-04 07:16:54 -0500

    taking data from front end and send it to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,33 @@ $(document).ready(init);
 
 function init() {
   console.log('Its alive');
+
+  $('#js-submit-task').on('submit', submitTask);
+}
+
+function submitTask(event) {
+  //event jQ gives us
+  event.preventDefault();
+
+  //capture input
+  const taskInput = $('#js-input-list').val();
+
+  postTask(taskInput); //gives the task input
+
+  clearTask(); // remember to call them once I pull out the thing
+}
+
+function postTask(task) {
+  const dataForServer = {
+    task: task,
+  }; //prepares the data, equals an object or the task. Value taken off taskInput. saved in task Input
+  $.ajax({
+    type: 'POST',
+    url: '/task',
+    data: dataForServer,
+  }); //taking data from he client and sends it to the server
+}
+
+function clearTask() {
+  $('#js-input-list').val(''); //clears input after entering so it doesn't show multiple times
 }
