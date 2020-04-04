ac591bfba926a91862426f19a1ba5d148eeec8ac
Derrick Idleburg Jr <derrick@primeacademy.io>
2020-04-04 07:28:08 -0500

    setting up the routes for the server

---
+++ server/routes/task.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+});
+
+router.get('/', (req, res) => {});
+
+router.put('/', (req, res) => {});
+
+router.delete('/', (req, res) => {});
+
+module.exports = router;
+//import this router
--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 //above are all import statements
 const PORT = process.env.PORT || 5000; //sets the port
 
+//routes
+const taskRouter = require('./routes/task.router');
+
 app.use(express.static('server/public')); //serves all static files
 app.use(bodyParser.urlencoded({ extended: true })); //allows me to use the Front End Created
 app.use(bodyParser.json()); //allows me to use Postman
+app.use('/task', taskRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
